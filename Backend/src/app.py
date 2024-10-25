4f2b8921b520a5dcdd4cfe665166da797b730cc4
Neal Paresh Patel <59032397+Desitrain22@users.noreply.github.com>
2024-10-25 17:13:36 -0400

    added endpoint for all stations at an hour

--- Backend/src/app.py
+++ Backend/src/app.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-from utils import get_station_ridership
 import pandas as pd
 import uvicorn
 
@@ -13,7 +12,16 @@ def read_root():
 
 @app.get("/station_ridership/{complex_id}/{hour}")
 def station_ridership(complex_id: str, hour: str):
-    return get_station_ridership(str(complex_id), str(hour), df)
+    df["hours"] = df["hours"].astype(str)
+    return dict(
+        df[(df["station_complex_id"] == complex_id) & (df["hours"] == hour)].iloc[0]
+    )
+
+
+@app.get("/all_stations/{hour}")
+def all_stations(hour: str):
+    df["hours"] = df["hours"].astype(str)
+    return dict(df[df["hours"] == hour].set_index("station_complex_id").T)
 
 
 if __name__ == "__main__":
