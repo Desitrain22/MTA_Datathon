9f8b7137af7e9a3e90b30a90795b4c6b017a9d8e
Neal Paresh Patel <59032397+Desitrain22@users.noreply.github.com>
2024-10-25 17:23:17 -0400

    added endpoint for accepting list of station IDs and getting responses

--- Backend/src/app.py
+++ Backend/src/app.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 import pandas as pd
 import uvicorn
+from fastapi import Request
 
 app = FastAPI()
 
@@ -22,6 +23,18 @@ def station_ridership(complex_id: str, hour: str):
 def all_stations(hour: str):
     df["hours"] = df["hours"].astype(str)
     return dict(df[df["hours"] == hour].set_index("station_complex_id").T)
+
+
+@app.get("/stations_at_hour/{hour}")
+def stations_at_hour(request: Request, hour: str):
+    body = request.json()
+    stations = body.get("stations", [])
+
+    filtered_df = df[df["station_complex_id"].isin(stations) & (df["hours"] == hour)][
+        ["station_complex_id", "sum_ridership"]
+    ].set_index("station_complex_id")
+
+    return dict(filtered_df.T)
 
 
 if __name__ == "__main__":
